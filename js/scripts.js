65140bfb3f352f082deb94d64c7e7158dc737947
Carli Martinez <carlimartinez@Carlis-iMac.local>
2015-02-13 12:01:19 -0800

    Add html page and triangleType function on form

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ var triangleType = function(side) {
   if (side[0] === (side[1] || side[2])) {
     if ((side[1] === side[2])) {
       return "equilateral"
-    } if ((side[0] === side[1]) && (side[2] >= (side[0] + side[1]))) {
+    } if ((side[0] === side[1]) && (side[2] > (side[0] + side[1]))) {
       return "not a triangle"
-    } else if ((side[0] === side[2]) && (side[1] >= (side[0] + side[2]))) {
+    } else if ((side[0] === side[2]) && (side[1] > (side[0] + side[2]))) {
       return "not a triangle"
     } else {
       return "isosceles"
@@ -15,3 +15,21 @@ var triangleType = function(side) {
     return "scalene"
   }
 }
+
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    sides = [];
+    var side_one = parseInt($("input#side_one").val());
+    var side_two = parseInt($("input#side_two").val());
+    var side_three = parseInt($("input#side_three").val());
+    sides.push(side_one);
+    sides.push(side_two);
+    sides.push(side_three);
+    var result = triangleType(sides);
+
+    $(".triangletype").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
