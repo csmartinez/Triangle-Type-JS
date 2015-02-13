7407562ae7ce565f98bd830e0c7219fd3370d40e
Carli Martinez <carlimartinez@Carlis-iMac.local>
2015-02-13 10:54:04 -0800

    Rewrite side function to include not a triangle detection

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@ var triangleType = function(side) {
   if (side[0] === (side[1] || side[2])) {
     if ((side[1] === side[2])) {
       return "equilateral"
+    } if ((side[0] === side[1]) && (side[2] >= (side[0] + side[1]))) {
+      return "not a triangle"
+    } else if ((side[0] === side[2]) && (side[1] >= (side[0] + side[2]))) {
+      return "not a triangle"
     } else {
       return "isosceles"
     }
-  } else if (side[1] === side[2]) {
+  } else if ((side[1] === side[2]) && (side[0] <= (side[1] + side[2]))) {
     return "isosceles"
   } else {
     return "scalene"
--- spec/specs.js
+++ spec/specs.js
@@ -2,10 +2,20 @@ describe('triangleType', function() {
   it('is scalene if no triangle sides are the same', function() {
     expect(triangleType([3, 4, 6])).to.equal("scalene");
   });
+
   it('is isosceles if any triangle sides are the same', function() {
     expect(triangleType([3, 6, 6])).to.equal("isosceles");
   });
+
+  it('is isosceles if any triangle sides are the same', function() {
+    expect(triangleType([7, 7, 6])).to.equal("isosceles");
+  });
+
   it('is equilateral if all triangle sides are the same', function() {
     expect(triangleType([18, 18, 18])).to.equal("equilateral");
+  });
+
+  it('it is not a triangle if you do not give numbers', function() {
+    expect(triangleType(["bark", "bark", "meow"])).to.equal("not a triangle");
   });
 });
